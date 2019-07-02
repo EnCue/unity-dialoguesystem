66519ff60298103f4211f9c8d00816f8d6ac1edd
Christopher <32796106+EnCue@users.noreply.github.com>
2019-07-02 14:38:55 +0800

    First step at preparing system deployment

---
+++ DictionaryConverter.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DictionaryConverter : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public Dictionary<string, string> sentences_Sample
+    {
+        get { return sentences_Sample; }
+        set { sentences_Sample = value; }
+    }
+    public Dictionary<string, string> responses_Sample
+    {
+        get { return responses_Sample; }
+        set { responses_Sample = value; }
+    }
+    public Dictionary<string, string> btnText_Sample
+    {
+        get { return btnText_Sample; }
+        set { btnText_Sample = value; }
+    }
+    /*public Dictionary<string, string> sentences_Siebel = new Dictionary<string, string>();
+    public Dictionary<string, string> sentences_Frosch = new Dictionary<string, string>();
+	public Dictionary<string, string> sentences_Altmayer = new Dictionary<string, string>();
+	public Dictionary<string, string> sentences_Brawler = new Dictionary<string, string>();
+    public Dictionary<string, string> responses_Siebel = new Dictionary<string, string>();
+    public Dictionary<string, string> responses_Frosch = new Dictionary<string, string>();
+	public Dictionary<string, string> responses_Altmayer = new Dictionary<string, string>();
+	public Dictionary<string, string> responses_Brawler = new Dictionary<string, string>();
+	public Dictionary<string, string[]> btnText_Siebel = new Dictionary<string, string[]>();
+	public Dictionary<string, string[]> btnText_Frosch = new Dictionary<string, string[]>();*/
+
+
+    void Awake()
+    {
+        //Provided example
+        Dialogue sampleDialogue = JsonUtility.FromJson<Dialogue>(File.ReadAllText(Application.streamingAssetsPath + "/SampleDialogue_dict.json"));
+        //Retrieving sentence information (dictionary tags and values)
+        string[] sTags_Sample = sampleDialogue.tags_NPC;
+        string[] sentences_Sample = sampleDialogue.Sentences;
+        //Retrieving response information (dictionary tags and values)
+        string[] rTags_sample = sampleDialogue.tags_Usr;
+        string[] responses_Sample = sampleDialogue.Responses;
+        //Retrieving interaction button information (dictionary tags and values)
+        string[] bTags_Sample = siebelDialogue.tags_bText;
+        string[] buttonTitle_Siebel = siebelDialogue.btnText;
+
+        //Constructing Unity dictionaries with information from json file above
+
+        //Compiling dictionary for sentences
+        sentences_Sample = compileDictionary(sTags_Sample, sentences_Sample);
+        //Compiling dictionary for responses
+        responses_Sample = compileDictionary(rTags_Sample, responses_Sample);
+        //Compiling dictionary for button entries
+        btnText_Sample = compileBtnDictionary(bTTags_Siebel, bText_Siebel);
+
+
+        /*Dialogue siebelDialogue = JsonUtility.FromJson<Dialogue>(File.ReadAllText(Application.streamingAssetsPath + "/SiebelDialogue.json"));
+        string[] dSTags_Siebel = siebelDialogue.tags_NPC;
+        string[] dRTags_Siebel = siebelDialogue.tags_Usr;
+        string[] dSentences_Siebel = siebelDialogue.Sentences;
+        string[] dResponses_Siebel = siebelDialogue.Responses;
+		string[] bText_Siebel = siebelDialogue.btnText;
+		string[] bTTags_Siebel = siebelDialogue.tags_bText;
+
+        Dialogue froschDialogue = JsonUtility.FromJson<Dialogue>(File.ReadAllText(Application.streamingAssetsPath + "/FroschDialogue.json"));
+        string[] dSTags_Frosch = froschDialogue.tags_NPC;
+        string[] dRTags_Frosch = froschDialogue.tags_Usr;
+        string[] dSentences_Frosch = froschDialogue.Sentences;
+        string[] dResponses_Frosch = froschDialogue.Responses;
+		string[] bText_Frosch = froschDialogue.btnText;
+		string[] bTTags_Frosch = froschDialogue.tags_bText;
+
+		Dialogue altmayerDialogue = JsonUtility.FromJson<Dialogue>(File.ReadAllText(Application.streamingAssetsPath + "/AltmayerDialogue.json"));
+        string[] dSTags_Altmayer = altmayerDialogue.tags_NPC;
+        string[] dRTags_Altmayer = altmayerDialogue.tags_Usr;
+        string[] dSentences_Altmayer = altmayerDialogue.Sentences;
+        string[] dResponses_Altmayer = altmayerDialogue.Responses;
+
+		Dialogue brawlerDialogue = JsonUtility.FromJson<Dialogue>(File.ReadAllText(Application.streamingAssetsPath + "/AltmayerDialogue.json"));
+        string[] dSTags_Brawler = brawlerDialogue.tags_NPC;
+        string[] dRTags_Brawler = brawlerDialogue.tags_Usr;
+        string[] dSentences_Brawler = brawlerDialogue.Sentences;
+        string[] dResponses_Brawler = brawlerDialogue.Responses;*/
+
+        /*sentences_Siebel = compileDDictionary (dSTags_Siebel, dSentences_Siebel, sentences_Siebel);
+		responses_Siebel = compileDDictionary (dRTags_Siebel, dResponses_Siebel, responses_Siebel);
+		btnText_Siebel = compileBtnDictionary (bTTags_Siebel, bText_Siebel, btnText_Siebel);
+		sentences_Frosch = compileDDictionary (dSTags_Frosch, dSentences_Frosch, sentences_Frosch);
+		responses_Frosch = compileDDictionary (dRTags_Frosch, dResponses_Frosch, responses_Frosch);
+		btnText_Frosch = compileBtnDictionary (bTTags_Frosch, bText_Frosch, btnText_Frosch);
+		sentences_Altmayer = compileDDictionary (dSTags_Altmayer, dSentences_Altmayer, sentences_Altmayer);
+		responses_Altmayer = compileDDictionary (dRTags_Altmayer, dResponses_Altmayer, responses_Altmayer);
+		sentences_Brawler = compileDDictionary (dSTags_Brawler, dSentences_Brawler, sentences_Brawler);
+		responses_Brawler = compileDDictionary (dRTags_Brawler, dResponses_Brawler, responses_Brawler);*/
+
+		//refLibrary lib = GameObject.FindGameObjectWithTag ("ManagerSystem").GetComponent<refLibrary> ();
+		//lib.dialogueCanvas = gameObject;
+		//lib.dDictionaryArchive = this;
+    }
+
+	private Dictionary<string, string> compileDictionary(string[] tags, string[] text, Dictionary<string, string> DictDump){
+        //Debug.Log (tags.Length);
+        //Debug.Log (text.Length);
+        Dictionary<string, string> DictDump = new Dictionary<string, string>();
+		for (int i = 0; i < tags.Length; i++) {
+			string indexedKey = tags[i];
+			string indexedElem = text[i];
+			DictDump.Add(indexedKey, indexedElem);
+		}
+
+		return DictDump;
+	}
+
+	private Dictionary<string, string[]> compileBtnDictionary(string[] tags, string[] text){
+        Dictionary<string, string> DictDump = new Dictionary<string, string>();
+		for (int i = 0; i < tags.Length; i++)
+		{
+			string indexedKey = tags[i];
+			string[] newEntries = new string[]{ };
+			try{
+				string[] entries = DictDump [indexedKey];
+				newEntries = new string[]{ entries [0], text [i] };
+
+				DictDump[indexedKey] = newEntries;
+			}catch(KeyNotFoundException){
+				newEntries = new string[]{ text [i] };
+
+				DictDump.Add(indexedKey, newEntries);
+			}
+		}
+
+		return DictDump;
+	}
+}
+
