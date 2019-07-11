d8f812b08d3462c623936a2406fcaa82dd287996
Christopher <Christopher.yq@gmail.com>
2019-07-11 10:54:38 +0800

    Assets

---
+++ Assets/Scripts/Dialogue System/NPCQuestManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCQuestManager : MonoBehaviour
+{
+    //ASSIGN TO INSTANCE OF DICTIONARYCONVERTER IN UNITY INSPECTOR
+    public DialogueManager dInterface;
+    public DictionaryConverter dRef;
+
+    private bool inputReceived;
+    private int inputReturn;
+	private bool canContinue;
+
+
+
+
+    void Awake() {
+        Messenger<int>.AddListener("contBtnRtrn", compileDecision);
+    }
+
+
+	//CALLED BY NPCS UPON USER INTERACTION
+    public IEnumerator qNPCDirectory(string caller)
+    {
+        
+		//Handler for NPC in provided example
+        if (caller == "sampleNPC_I")
+        {
+			//Basic template for NPC quest handler
+
+			//Suspends player movement during conversation
+			Messenger<bool>.Broadcast ("canMove_updated", false);
+
+			//Begins quest coroutine
+            StartCoroutine(sampleQuest(wasSuccessful => {
+				
+				Messenger<bool>.Broadcast("canMove_update", true);
+
+                if (!wasSuccessful) {
+                    Debug.Log("Failed quest; prompting restart.");
+                    //**HANDLER FOR QUEST FAILURE**
+               	} else {
+                    Debug.Log("Quest succesfully completed; returning to start");
+                    //PROCEED
+                }
+            }));
+        }
+        else
+        {
+            //ADD PATHS FOR ADDITIONAL NPCs
+        }
+
+        yield return null;
+    }
+
+
+
+    private void compileDecision(int btnSelection)
+    {
+        inputReceived = true;
+        inputReturn = btnSelection;
+    }
+
+
+	//Sample quest
+    private IEnumerator sampleQuest(Action<bool> Accomplished)
+    {
+		//Generic conversational quest
+
+		//Retrieving dialogue info
+		Dictionary<string, string> dRef_NPC = dRef.sample_sDict;
+		Dictionary<string, string> dRef_User = dRef.sample_rDict;
+        Dictionary<string, string[]> dRef_btnTxt = dRef.btnText_Sample;
+
+        Debug.Log("Engaged Sample NPC");
+        string NPCname = "NPC_I";
+        string branchPath = "smplDialogue";
+        string[] contOptions = new string[2];
+        bool qFailed = false;
+
+        while (true)
+		{
+            string[] newOration = dInterface.getOration(branchPath, dRef_NPC);
+            contOptions = dRef_btnTxt[branchPath];
+            dInterface.outputDialogue(NPCname, newOration, contOptions);
+
+            inputReceived = false;
+            yield return new WaitUntil(() => inputReceived);
+
+            int qBranch = inputReturn;
+            if (qBranch == 2)
+            {
+                if (!qFailed)
+                {
+                    Debug.Log("Quest Complete");
+                    dInterface.EndDialogue();
+                    //qStatus.qPhase [0] = 2;
+
+                    Accomplished(true);
+                    yield break;
+                }
+                else
+                {
+                    Debug.Log("Quest Failed");
+                    dInterface.EndDialogue();
+                    //qStatus.qPhase [0] = 2;
+
+                    Accomplished(false);
+                    yield break;
+                }
+            }
+            branchPath = branchPath + qBranch.ToString();
+            canContinue = false;
+            StartCoroutine(dInterface.displayResponse(dRef_User, branchPath, conGate => (canContinue = conGate)));
+            yield return new WaitUntil(() => canContinue);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public static float moveSpeed = 3f;
+
+	public int rotation;
+    public Vector3 Orientation;
+	private Vector2 initialPos;
+	private Vector2 Destination;
+
+    private Animator anim;
+    private bool canMove;
+    public bool playerMoving;
+
+    private Rigidbody2D rbody;
+    Vector2 velocity;
+
+    private float timeDownX = 0.0f; // time which horizontal was pressed
+    private float timeDownY = 0.0f; // time which vertical was pressed
+
+    //Singleton pattern
+    static PlayerController Instance; 
+
+    void Awake()
+    {
+        //Checks if the instaniated object is a copy
+        /* if (Instance != null)
+        {
+            //Commit suicide if is it a copy
+            Destroy(this.gameObject);
+            return; 
+        }
+        //Instance is set to be the singelton if not a copy
+        Instance = this; 
+		GameObject.DontDestroyOnLoad (this.gameObject);
+		rotation = 90;*/
+    }
+    void Start()
+    {
+		Messenger<bool>.AddListener ("canMove_Update", setCanMove);
+		canMove = true;
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    public void setCanMove(bool canMove_setter){
+		canMove = canMove_setter;
+	}
+
+
+    void FixedUpdate()
+    {
+		//Get Input
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputY = Input.GetAxisRaw("Vertical");
+		if (canMove) {
+            //if key is pressed on Horizontal save time it was pressed
+            if (inputX != 0)
+            {
+                if (timeDownX == 0.0f) //if there is no stored time for it
+                    timeDownX = Time.time;
+            }
+            else
+            {
+                timeDownX = 0.0f; // reset time if no button is being pressed
+            }
+
+            //if key is pressed on vertical save time it was pressed
+            if (inputY != 0)
+            {
+                if (timeDownY == 0.0f) //if there is no stored time for it
+                    timeDownY = Time.time;
+            }
+            else
+            {
+                timeDownY = 0.0f; // reset time if no button is being pressed
+            }
+           
+            //check which button was hit last to determin direction
+            velocity = Vector2.zero;
+			if (timeDownX > timeDownY)
+            {
+                velocity = Vector2.right * inputX;
+            }
+			else if (timeDownX < timeDownY) 
+			{
+				velocity = Vector2.up * inputY;
+			}
+
+			if (inputX != 0 && inputY != 0) {
+				if (timeDownX == timeDownY) {
+					inputX = 0;
+				} else if (timeDownX > timeDownY) {
+					inputY = 0;
+				} else if (timeDownY > timeDownX) {
+					inputX = 0;
+				}
+			}
+			if (inputX > 0) {
+				Orientation = Vector3.right;
+				rotation = 0;
+			} else if (inputX < 0) {
+				Orientation = -Vector3.right;
+				rotation = 180;
+			}
+			if (inputY > 0) {
+				Orientation = Vector3.up;
+				rotation = 90;
+			} else if (inputY < 0) {
+				Orientation = -Vector3.up;
+				rotation = 270;
+			}
+
+            // Animations
+            playerMoving = (Mathf.Abs(inputX) + Mathf.Abs(inputY)) > 0;
+            anim.SetBool("PlayerMoving", playerMoving);
+
+            if (playerMoving)
+            {
+				anim.SetFloat ("MoveX", inputX);
+				anim.SetFloat ("MoveY", inputY);
+            }
+
+            // Setting the veloctiy based on input 
+			/*if (movementException) {
+				rbody.MovePosition(rbody.position + (offsetVector * (Time.fixedDeltaTime / distRatio)));
+			} else{
+				rbody.MovePosition(rbody.position + (velocity * moveSpeed) * Time.fixedDeltaTime);
+			}*/
+
+			rbody.MovePosition(rbody.position + (velocity * moveSpeed) * Time.fixedDeltaTime);
+		}
+    }
+}
