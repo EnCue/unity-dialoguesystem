a77b3afba270d1eee55c2b0429dd4e72f567f055
Christopher <32796106+EnCue@users.noreply.github.com>
2019-07-11 11:00:11 +0800

    Revised sample Unity project

---
+++ Sample Project/Assets/Scripts/Dialogue System/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
+
+[System.Serializable]
+public class Dialogue { 
+    public string Name; 
+    public string[] tags_NPC;
+    public string[] tags_Usr;
+    public string[] Sentences;
+    public string[] Responses;
+	public string[] btnText;
+	public string[] tags_bText;
+}
